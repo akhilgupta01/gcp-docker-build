0a0a4fa8afa72d0ab3e8026c3aad6dc63a13fca6
Akhil Gupta <akhilgupta01@gmail.com>
2021-08-02 09:02:34 +0530

    first commit

---
+++ src/main/java/com/guptakh/gcp/GcpDockerBuildApplication.java
@@ -0,0 +1,13 @@
+package com.guptakh.gcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpDockerBuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpDockerBuildApplication.class, args);
+	}
+
+}
