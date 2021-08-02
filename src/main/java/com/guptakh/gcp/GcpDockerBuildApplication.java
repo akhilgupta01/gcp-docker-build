6c2d401b10608a6d399431a819b5f50137ab1f1b
Akhil Gupta <akhilgupta01@gmail.com>
2021-08-02 10:24:26 +0530

    batch run

--- src/main/java/com/guptakh/gcp/GcpDockerBuildApplication.java
+++ src/main/java/com/guptakh/gcp/GcpDockerBuildApplication.java
@@ -1,16 +1,21 @@
 package com.guptakh.gcp;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-public class GcpDockerBuildApplication implements CommandLineRunner {
+public class GcpDockerBuildApplication {
+	public static void main(String[] args) {
+		SpringApplication application = new SpringApplicationBuilder()
+				.sources(GcpDockerBuildApplication.class)
+				.web(WebApplicationType.NONE)
+				.build();
 
-	@Override
-	public void run(String... args) throws InterruptedException {
-		System.out.println("The application started");
-		Thread.sleep(5000);
+		ConfigurableApplicationContext applicationContext = application.run(args);
+		int exitCode = SpringApplication.exit(applicationContext);
+		System.exit(exitCode);
 	}
-
 }
