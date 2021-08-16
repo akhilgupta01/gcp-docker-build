dd9e82e48920ed5f4b0d238beeeeda47c598b63a
Akhil Gupta <akhilgupta01@gmail.com>
2021-08-16 16:05:14 +0530

    added app.yaml

--- src/main/java/com/guptakh/gcp/GcpDockerBuildApplication.java
+++ src/main/java/com/guptakh/gcp/GcpDockerBuildApplication.java
@@ -8,13 +8,15 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class GcpDockerBuildApplication {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication application = new SpringApplicationBuilder()
 				.sources(GcpDockerBuildApplication.class)
-				//.web(WebApplicationType.NONE)
 				.build();
 
 		ConfigurableApplicationContext applicationContext = application.run(args);
+		System.out.println("Starting sleep");
+		Thread.sleep(2000);
+		System.out.println("Out of sleep");
 		int exitCode = SpringApplication.exit(applicationContext);
 		System.exit(exitCode);
 	}
